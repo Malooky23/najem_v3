4fa6bce72590de4d939861ff4437c7ebc48e6121
malooky23 <malooky23@gmail.com>
2025-03-18 23:40:02 +0100

    refactor: update mobile bottom navigation links and icons

--- src/app/(pages)/(protected)/warehouse/items/components/data-table/mobile/mobile-bottom-nav.tsx
+++ src/app/(pages)/(protected)/warehouse/items/components/data-table/mobile/mobile-bottom-nav.tsx
@@ -1,6 +1,6 @@
 import CreateItemForm from "../../CreateItem"
 import { Button } from "@/components/ui/button"
-import { Home, List, ListFilter, Plus, Search, Settings } from "lucide-react"
+import { GitCompareArrowsIcon, Home, List, ListFilter, Plus, Search, Settings, UserCog2 } from "lucide-react"
 import Link from "next/link"
 // import { useRouter } from "next/navigation"
 
@@ -10,34 +10,34 @@ export function MobileBottomNav() {
     <div className="fixed bottom-0 left-0 right-0 border-t bg-background z-10 pb-2">
       <div className="flex justify-around items-center h-16">
 
-        <Button variant="ghost" className="flex flex-col h-full rounded-none flex-1 items-center justify-center"
+        <Link href="/warehouse" className="flex flex-col h-full rounded-none flex-1 items-center justify-center"
         // onClick={() => router.push("/warehouse")}
         >
 
           <Home className="h-5 w-5" />
-            <span className="text-xs mt-1">Home</span>
+          <span className="text-xs mt-1">Home</span>
 
-        </Button>
+        </Link>
 
-        <Button variant="ghost" className="flex flex-col h-full rounded-none flex-1 items-center justify-center"
-                // onClick={() => router.push("/warehouse/orders")}
->
-          <List width={24} />
+        <Link href="/warehouse/orders" className="flex flex-col h-full rounded-none flex-1 items-center justify-center "
+        // onClick={() => router.push("/warehouse/orders")}
+        >
+          <List className="h-5 w-5"  />
           <span className="text-xs mt-1">Orders</span>
-        </Button>
+        </Link>
         {/* <Button className="h-12 w-12 rounded-full absolute -top-6 left-1/2 transform -translate-x-1/2 flex items-center justify-center">
           <Plus className="h-6 w-6" />
         </Button> */}
         <CreateItemForm />
         <div className="flex-1" /> {/* Spacer */}
-        <Button variant="ghost" className="flex flex-col h-full rounded-none flex-1 items-center justify-center">
-          <Search className="h-5 w-5" />
-          <span className="text-xs mt-1">Search</span>
-        </Button>
-        <Button variant="ghost" className="flex flex-col h-full rounded-none flex-1 items-center justify-center">
-          <Settings className="h-5 w-5" />
-          <span className="text-xs mt-1">Settings</span>
-        </Button>
+        <Link href="/customers" className="flex flex-col h-full rounded-none flex-1 items-center justify-center">
+          <UserCog2 className="h-5 w-5" />
+          <span className="text-xs mt-1">Customers</span>
+        </Link>
+        <Link href="/warehouse/tx" className="flex flex-col h-full rounded-none flex-1 items-center justify-center">
+          <GitCompareArrowsIcon className="h-5 w-5" />
+          <span className="text-xs mt-1"> Movements</span>
+        </Link>
       </div>
     </div>
   )
